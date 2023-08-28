4b2c49531426bcacf0e00b3ebfdfb67f88e10b0a
shivanshss <48706782+shivanshss@users.noreply.github.com>
2023-08-28 20:35:11 +0530

    Create split_fasta_by_name.py

---
+++ split_fasta_by_name.py
@@ -0,0 +1,20 @@
+## Usgae split_fasta_by_name.py <input.fasta> <names_to_remove.txt> > <filtered.fasta>
+
+#!/usr/bin/env python3
+
+
+
+from Bio import SeqIO
+import sys
+
+ffile = SeqIO.parse(sys.argv[1], "fasta")
+header_set = set(line.strip() for line in open(sys.argv[2]))
+
+for seq_record in ffile:
+    try:
+        header_set.remove(seq_record.name)
+    except KeyError:
+        print(seq_record.format("fasta"))
+        continue
+if len(header_set) != 0:
+    print(len(header_set),'of the headers from list were not identified in the input fasta file.', file=sys.stderr)
